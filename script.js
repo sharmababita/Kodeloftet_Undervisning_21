99683031243d7ea6e996bf7cc5d4ddba07f5fd4b
sharmababita <sharma.089babita@gmail.com>
2025-07-10 11:26:21 +0200

    catFact

--- script.js
+++ script.js
@@ -20,3 +20,40 @@ sunIcon.addEventListener("click", function () {
   body.classList.remove("light");
   body.classList.add("dark");
 });
+
+//
+//
+
+async function fetchCatFacts() {
+  try {
+    // All JS som skal skje i funksjonen
+    const response = await fetch("https://catfact.ninja/facts");
+    console.log(response);
+
+    const data = await response.json();
+    //console.log(data.data[4].fact);
+    //console.log(data);
+
+    return data;
+} catch (error) {
+  //feilmelding
+  console.error(`${error}: Noe galt har skjedd.`)
+}
+}
+
+async function displayAllFacts() {
+  const catFacts = await fetchCatFacts();
+  console.log(catFacts);
+
+  const factList = document.querySelector("#factList");
+  console.log(factList);
+
+  catFacts.data.forEach((item) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = item.fact;
+
+  factList.appendChild(listItem);
+  }) 
+};
+
+displayAllFacts();
